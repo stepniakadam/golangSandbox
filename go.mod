f9d963bd4d129dcd5d395cd95c051fc6b65c38d2
adamstepniak41 <adamstepniak41@github.com>
2021-12-12 18:45:03 +0100

    Initial commit of golang sandbox

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/adamstepniak41/golangSandbox
+
+go 1.16
+
+require rsc.io/quote v1.5.2
---
+++ hello_test.go
@@ -0,0 +1,12 @@
+package hello
+
+import "testing"
+import "fmt"
+
+func TestHello(t *testing.T) {
+    want := "Hello, world."
+    if got := Hello(); got != want {
+        t.Errorf("Hello() = %q, want %q", got, want)
+    }
+    fmt.Printf("test")
+}
